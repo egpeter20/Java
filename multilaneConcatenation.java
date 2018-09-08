484e4536784556f22b7d2efea80b1a6244f4a1d5
Peter Botros <peter.harby1@gmail.com>
2018-09-07 18:30:29 -0700

    Create multilaneConcatenation.java
    Multi-lane Concatenation

---
+++ multilaneConcatenation.java
@@ -0,0 +1,15 @@
+package as1;
+
+import javax.swing.JOptionPane; // Library
+
+public class Message {
+
+	public static void main(String[] args) {
+		
+		String out;
+		out="Today is a good day.\n";
+		out=out + "I am learning Java programming.\n";
+		out=out + "It’s a lot of fun.\n";
+		JOptionPane.showMessageDialog (null, out);
+	}
+}
