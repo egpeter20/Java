cc24b28b77b095c345a78798864caede1cce5f72
Peter Botros <peter.harby1@gmail.com>
2018-09-14 19:15:04 -0700

    Create TestRectangle.java

---
+++ TestRectangle.java
@@ -0,0 +1,55 @@
+/**********************************************************
+ * TestRectangle class it tests the Rectangle class
+**********************************************************/
+package as4;
+
+import javax.swing.JOptionPane;
+
+public class TestRectangle {
+	public static void main(String[] args) {
+		Rectangle rec1, rec2;
+		String in, out;
+		
+		in = JOptionPane.showInputDialog("Enter Length");
+		double len = Double.parseDouble(in);
+		in = JOptionPane.showInputDialog("Enter width");
+		double wid = Double.parseDouble(in);
+		rec1 = new Rectangle(len, wid);
+		
+		in = JOptionPane.showInputDialog("Enter Length");
+		len = Double.parseDouble(in);
+		in = JOptionPane.showInputDialog("Enter width");
+		wid = Double.parseDouble(in);
+		rec2 = new Rectangle(len,wid);
+		
+		// Using first object
+		double area = rec1.findArea();
+		double perim = rec1.findPerim();
+		len = rec1.getLength();
+		wid = rec1.getWidth();
+		
+		// Build the output for the first object
+		out  = "First Rectangle\n";
+		out += "Length: " + len + "\n";
+		out += "Width: " + wid + "\n";
+		out += "Area: " + area + "\n";
+		out += "Perimeter: " + perim + "\n";
+				
+		// Using Second object
+		area = rec2.findArea();
+		perim = rec2.findPerim();
+		len = rec2.getLength();
+		wid = rec2.getWidth();
+		
+		// Build the output for the second object
+		out += "\nSecond Rectangle\n";
+		out += "Length: " + len + "\n";
+		out += "Width: " + wid + "\n";
+		out += "Area: " + area + "\n";
+		out += "Perimeter: " + perim + "\n";
+		
+		// Display the output
+		JOptionPane.showMessageDialog(null, out);
+		
+		}
+}
