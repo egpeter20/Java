ad210b59643a9bd0e700c44b48513bea43c28f97
Peter Botros <peter.harby1@gmail.com>
2018-10-12 18:23:29 -0700

    Create TestSiblingExt.java

---
+++ TestSiblingExt.java
@@ -0,0 +1,112 @@
+// This project demonstrate the use of inheritance
+// TestSiblingExt Class
+// Sibling class
+// SiblingExt class
+package as9;
+
+import javax.swing.JOptionPane;
+
+public class TestSiblingExt {
+
+	public static void main(String[] args) {
+		String in, out, name;
+		int age, weight, height;
+		
+		name = JOptionPane.showInputDialog("Enter Name: ");
+		in = JOptionPane.showInputDialog("Enter Age: ");
+		age= Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Weight: ");
+		weight = Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Height: ");
+		height = Integer.parseInt(in);
+		SiblingExt sibe1 = new SiblingExt(name, age, weight, height);
+		
+		name = JOptionPane.showInputDialog("Enter Name: ");
+		in = JOptionPane.showInputDialog("Enter Age: ");
+		age= Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Weight: ");
+		weight = Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Height: ");
+		height = Integer.parseInt(in);
+		SiblingExt sibe2 = new SiblingExt(name, age, weight, height);
+		
+		name = JOptionPane.showInputDialog("Enter Name: ");
+		in = JOptionPane.showInputDialog("Enter Age: ");
+		age= Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Weight: ");
+		weight = Integer.parseInt(in);
+		in = JOptionPane.showInputDialog("Enter Height: ");
+		height = Integer.parseInt(in);
+		SiblingExt sibe3 = new SiblingExt(name, age, weight, height);
+		
+		// Find the youngest
+		SiblingExt youngest = null;
+		
+		if(sibe1.getAge() <= sibe2.getAge() && 
+			sibe1.getAge() <= sibe3.getAge()) {
+			youngest = sibe1;
+		}
+		else if (sibe2.getAge() <= sibe1.getAge() && 
+				sibe2.getAge() <= sibe3.getAge()) {
+			youngest = sibe2;
+		}
+		else {
+			youngest = sibe3;
+		} // End of youngest sequence
+		
+		// Find the lightest
+		SiblingExt lightest = null;
+		
+		if(sibe1.getWeigth() <= sibe2.getWeigth() && 
+			sibe1.getWeigth() <= sibe3.getWeigth()) {
+			lightest = sibe1;
+		}
+		else if (sibe2.getWeigth() <= sibe1.getWeigth() && 
+				sibe2.getWeigth() <= sibe3.getWeigth()) {
+				lightest = sibe2;
+		}
+		else {
+			lightest = sibe3;
+		} // End of lightest sequence
+		
+		// Find the tallest
+		SiblingExt tallest = null;
+		
+		if(sibe1.getHeight() >= sibe2.getHeight() && 
+			sibe1.getHeight() >= sibe3.getHeight()) {
+			tallest = sibe1;
+		}
+		else if (sibe2.getHeight() >= sibe1.getHeight() && 
+				sibe2.getHeight() >= sibe3.getHeight()) {
+				tallest = sibe2;
+		}
+		else {
+			tallest = sibe3;
+		} // End of tallest sequence
+		
+		// The output
+		// Building the lightest output
+		out = "The lightest sibling " 
+			+lightest.getName()+" "
+			+lightest.getAge()+" "
+			+lightest.getWeigth()+" "
+			+lightest.getHeight()+"\n";
+		
+		// Building the youngest output
+		out+= "The youngest sibling " 
+			+youngest.getName()+" "
+			+youngest.getAge()+" "
+			+youngest.getWeigth()+" "
+			+youngest.getHeight()+"\n";
+		
+		// Building the tallest output
+		out+= "The tallest sibling "			
+			+tallest.getName()+" "
+			+tallest.getAge()+" "
+			+tallest.getWeigth()+" "
+			+tallest.getHeight()+"\n";
+		
+		// Display output
+		JOptionPane.showMessageDialog(null, out);
+	}
+}
