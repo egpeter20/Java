578b174dfe17fa879fd84abcf40972cb689b35f8
Peter Botros <peter.harby1@gmail.com>
2018-10-12 18:23:46 -0700

    Create Sibling.java

---
+++ Sibling.java
@@ -0,0 +1,29 @@
+// Sibling class
+package as9;
+
+public class Sibling {
+	private String name;
+	private int age;
+	private int weigth;
+	
+	// Main Constructor!
+	public Sibling(String name, int age, int weigth) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.weigth = weigth;
+	}
+		
+	public String getName() {
+		return name;
+	}
+		
+	public int getAge() {
+		return age;
+	}
+		
+	public int getWeigth() {
+		return weigth;
+	}
+	
+}
